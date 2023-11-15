f8ecabb34b2fa40d53e156d5a15f149a90a90861
Anastasia <Silaevanu@mail.ru>
2023-11-15 20:23:00 +0300

    111

---
+++ part_001/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("введите первое число:");
+int value1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число:");
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+int result = value1 + value2;
+Console.WriteLine(result);
