a94a0a747b5d7743eebc622bac898c757c075462
Anastasia <Silaevanu@mail.ru>
2023-11-15 21:15:18 +0300

    333

---
+++ part_003/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.CursorVisible = false;
+//Console.SetCursorPosition(30, 4);
+
+int left1 = 2;
+int top1 = 28;
+Console.SetCursorPosition(left1, top1);
+Console.WriteLine("+");
+
