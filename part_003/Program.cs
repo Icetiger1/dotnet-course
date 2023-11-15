c7226ae6d9cbd15a23b93547f45ea03aff0b48c4
Anastasia <Silaevanu@mail.ru>
2023-11-15 21:16:59 +0300

    444

--- part_003/Program.cs
+++ part_003/Program.cs
@@ -2,9 +2,17 @@
 Console.Clear();
 Console.CursorVisible = false;
 //Console.SetCursorPosition(30, 4);
-
-int left1 = 2;
-int top1 = 28;
+int left1 = 1;
+int top1 = 40;
 Console.SetCursorPosition(left1, top1);
 Console.WriteLine("+");
 
+int left2 = 2;
+int top2 = 28;
+Console.SetCursorPosition(left1, top1);
+Console.WriteLine("+");
+
+int left2 = 80;
+int top2 = 28;
+Console.SetCursorPosition(left1, top1);
+Console.WriteLine("+");
