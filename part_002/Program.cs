1ffbfa0e4db065d590b4fa0563fd20c5c195d811
Anastasia <Silaevanu@mail.ru>
2023-11-15 21:07:34 +0300

    часть 2

---
+++ part_002/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Write("Введите Ваше имя: ");
+
+string fullName = Console.ReadLine();
+
+if (fullName == "Admin")
+{
+    Console.WriteLine("Привет");
+
+}
+else
+{
+    Console.WriteLine($"Я так ждала Вас, {fullName}");
+}
