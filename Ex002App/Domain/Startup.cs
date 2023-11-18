5f3f38112865a8a65c377ccc735ecb85b8050d67
Anastasia <Silaevanu@mail.ru>
2023-11-18 14:54:20 +0300

    1

--- Ex002App/Domain/Startup.cs
+++ Ex002App/Domain/Startup.cs
@@ -6,8 +6,8 @@ public class Startup
 
     public static void Run()
     {
-        RepositoryPrint repositoryPrint = new;
-        ContactCreator contactCreator = new;
+        RepositoryPrint repositoryPrint = new();
+        ContactCreator contactCreator = new();
         contactRepository repo = new();
 
         for (int i = 0; i < 10; i++)
